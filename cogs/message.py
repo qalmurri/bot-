c65039697d21c0c348a663dcab8758b833e4b6a4
qalmurri <86642474+qalmurri@users.noreply.github.com>
2023-09-26 03:11:45 +0700

    Add files via upload

--- cogs/message.py
+++ cogs/message.py
@@ -1,4 +1,3 @@
-import discord
 from discord.ext import commands
 
 class message(commands.Cog):
@@ -7,14 +6,21 @@ class message(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        print(f'{message.guild.id}/{message.channel.id}')
-        print(f'{message.author.id}: {message.content}')
-        await self.bot.process_commands(message)
+        if message.reference:
+            a = await message.channel.fetch_message(message.reference.message_id)
+            if a:
+                print(f"{message.guild.id}/{message.channel.id}/{message.id}")
+                print(f"{a.author.id}: {a.content} < {message.author.id}: {message.content}")
+        else:
+            print(f'{message.guild.id}/{message.channel.id}')
+            print(f'{message.author.id}: {message.content}')
+            await self.bot.process_commands(message)
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
-        print(f'{reaction.message.guild.id}/{reaction.message.channel.id}/{reaction.message.id} ')
-        print(f"{user.id} {reaction.emoji} {reaction.message.author.id}")
+        a = reaction.message
+        print(f'{a.guild.id}/{a.channel.id}/{a.id} ')
+        print(f"{a.author.id}: {a.content} < {user.id}: {reaction.emoji}")
 
 async def setup(bot):
     await bot.add_cog(message(bot))
--- main.py
+++ main.py
@@ -4,18 +4,18 @@ import traceback
 import sys
 
 bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
-extensions = ['cogs.message', 'cogs.threads', 'cogs.voice']
+a = ['cogs.message', 'cogs.voice']
 
 @bot.event
 async def on_ready():
-    print(bot.user.name + " (" + str(bot.user.id) + ") ON")
-    
-    for extension in extensions:
+    print(f'{bot.user.name} ({bot.user.id}) on {len(bot.guilds)} Server')
+
+    for b in a:
         try:
-            await bot.load_extension(extension)
-            print(f'Memuat {extension} : OK')
+            await bot.load_extension(b)
+            print(f'Load {b}: Ok')
         except Exception as e:
-            print(f'Memuat {extension} : GAGAL.', file=sys.stderr)
+            print(f'Load {b}: Error.', file=sys.stderr)
             traceback.print_exc()
 
 bot.run("MTE1MzMyNjY3NjUxMjYxNjQ2OQ.GBuFwf.kOKKr8d2NW15deATZuRC6OkZFgyFuh5XoprIYc")
